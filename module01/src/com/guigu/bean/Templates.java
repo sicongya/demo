4432f8192c32e2da40f596d501d122a1311d7b7a
sicong <1670512231@qq.com>
2020-07-28 20:54:14 +0800

    测试提交

---
+++ module01/src/com/guigu/bean/Templates.java
@@ -0,0 +1,30 @@
+package com.guigu.bean;
+
+import java.util.Date;
+
+/**
+ * @auther sicong
+ * @date 2020/7/28 - 15:15
+ */
+public class Templates {
+    public static void main(String[] args) {
+        int a = 100;
+        for (int i = 0; i < a; i++) {
+
+            for (int j = 0; j < a; j++) {
+
+                for (int k = 0; k < a; k++) {
+                    
+                }
+            }
+        }
+        Date date = new Date();
+        long time = date.getTime();
+        for (int i = 0; i < a; i++) {
+            
+        }
+    }
+
+    System.out.print("红红火火恍恍惚惚");
+
+}
